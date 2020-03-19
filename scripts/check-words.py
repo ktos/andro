46b2469894e543e87d192ee90e2f9f3502200c24
Marcin Badurowicz <m@badurowicz.net>
2020-03-19 21:48:19 +0100

    Add GitHub actions support (#1)
    Add generating PDF, MD files and checking words for proper syllabary on every master push and pull request

--- scripts/check-words.py
+++ scripts/check-words.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import dictionaryparser
+import sys
 
 # read and parse dictionary file
 words = dictionaryparser.read_dictionary('../dictionary.csv')
@@ -9,11 +10,16 @@ words = dictionaryparser.read_dictionary('../dictionary.csv')
 # all syllabes
 alls = [line.rstrip() for line in open('./syllabes.txt', encoding='utf-8')]
 
+# strict mode: fail on first problem and return exitcode 1 so CI will know
+# about it
+strict = True if len(sys.argv) > 1 and sys.argv[1] == "strict" else False
+
 def check_syllabes(speech):
     l = speech.strip().replace("ˈ", "")
     for i in l.split('.'):
         if i not in alls:
             print(f"SYLLABLE WARN: {l} (because of {i})")
+            if strict: exit(1)
 
 # check all words except empty speech and names for proper syllabes
 for i in filter(lambda x: x['speech'] != '' and x['type'] != 'name', words):
