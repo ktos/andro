13fd188a0c99ccbea0940f42cfc26305411194ea
Marcin Badurowicz <m@badurowicz.net>
2020-03-20 18:36:56 +0100

    Fix scripts for sorting CSVs

--- sort-csv.py
+++ sort-csv.py
@@ -2,12 +2,15 @@
 # coding: utf-8
 
 import unidecode
+import sys
+
+filename = sys.argv[1] if len(sys.argv) > 1 else 'dictionary.csv'
 
 # reads dictionary descfile
-with open('../dictionary.csv', 'r', encoding='utf-8') as f:
+with open(filename, 'r', encoding='utf-8') as f:
     data = f.readlines()
 
 sorted_lines = sorted(data, key=lambda x: unidecode.unidecode(x.split('|')[0]))
 
-with open('../dictionary.csv', 'w', encoding='utf-8') as f:
+with open(filename, 'w', encoding='utf-8') as f:
     f.writelines(sorted_lines)
