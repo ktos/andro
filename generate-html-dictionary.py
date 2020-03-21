5552819fa2cf0a2e4a3e07c525bbc6420c1044bc
Marcin Badurowicz <m@badurowicz.net>
2020-03-21 01:14:02 +0100

    Tweak Polish HTML template

--- generate-html-dictionary.py
+++ generate-html-dictionary.py
@@ -6,44 +6,74 @@ import shutil
 import sys
 import dictionaryparser
 
-def inject_name(name):
-    with open(name + "/index.html", "r+") as f:
-        data = f.read()
-        f.seek(0)
-        f.write(data.replace("<!-- NAME -->", name, 2))
-        f.truncate()
+def format_description(desc):
+    desc = desc.replace("<see>", '<a class="see" href="#">')
+    desc = desc.replace("</see>", '</a>')
 
-def copy_template(name):
+    desc = desc.replace("<phrase>", "<em>")
+    desc = desc.replace("</phrase>", "</em>")
+
+    desc = desc.replace("<em>", "<em>")
+    desc = desc.replace("</em>", "</em>")
+
+    desc = desc.replace("<alt>", "<em>")
+    desc = desc.replace("</alt>", "</em>")
+
+    desc = desc.replace("<sc>", '<span class="sc">')
+    desc = desc.replace("</sc>", "</span>")
+
+    desc = desc.replace("--", "—")
+    desc = desc.replace(",,", "„")
+    desc = desc.replace("''", "”")
+    desc = desc.replace("~", "&nbsp;")
+    desc = desc.replace("\-", "")
+
+    return desc
+
+def copy_template(lang):
+    name = f"final/html-{lang}/"
+    template = f"html-{lang}-template"
+
     if not os.path.exists(name):
         os.makedirs(name)
-    for filename in os.listdir("template"):
-        target = name + "/" + filename
-        templateFile = "template/" + filename
+    for filename in os.listdir(template):
+        target = name + filename
+        templateFile = template + "/" + filename
         if os.path.isdir(templateFile):
-            if os.path.exists(target): shutil.rmtree(target)
-            shutil.copytree(templateFile, target) # copy template
+            if os.path.exists(target):
+                shutil.rmtree(target)
+            shutil.copytree(templateFile, target)  # copy template
         else:
-            if os.path.exists(target): os.remove(target)
-            shutil.copy(templateFile, target) # copy template
+            if os.path.exists(target):
+                os.remove(target)
+            shutil.copy(templateFile, target)  # copy template
+
 
 def generate_words(rows):
-    words = []    
+    words = []
 
     for i, row in enumerate(rows):
-        if (i == 0): continue
-        
         word = {
-                "id":          i - 1,
-                "word":        row['word'],
-                "english":     row['description'],
-                "types":       [row['type']],
-                "ipa":         row['speech'],
-                "definitions": [row['notes']],
-                "examples":    []
-                }
-        
+            "id": i,
+            "word": row['word'],
+            "translation": format_description(row['description']),
+            "type": row['type'],
+            "ipa": row['speech'],
+            "notes": [],
+            "examples": row['examples']
+        }
+
+        for j in row['notes']:
+            word['notes'].append(format_description(j))
+
+        for j in ['pl', 'fem', 'pst', 'comp', 'supl']:
+            if j in row:
+                word[j] = row[j]
+                word[j + "_speech"] = row[j + "_speech"]
+
         words.append(word)
     return words
+
 
 def getRowsFromFile(fileLocation):
     with open(fileLocation, "r") as f:
@@ -51,17 +81,22 @@ def getRowsFromFile(fileLocation):
 
 
 def main():
-    fileLocation = sys.argv[1]
-    name = ntpath.basename(fileLocation).split(".")[0]
+    if len(sys.argv) > 1:
+        lang = sys.argv[1]
+    else:
+        lang = 'pl'
 
-    dictionary = dictionaryparser.read_dictionary('../dictionary.csv')    
-    words = generate_words(filter(lambda x: x['type'] not in ['name', 'phraseology'], dictionary))
-    copy_template(name)
+    dictionary = dictionaryparser.read_dictionary('dictionary.csv')
+    words = generate_words(filter(lambda x: x['type'] not in [
+                           'name', 'phraseology'], dictionary))
+    copy_template(lang)
 
-    with open(name + '/scripts/words.js', 'w') as file:
-        file.write("const words = " + json.dumps(words, separators=(',', ':')) + ";")
-    inject_name(name)
-    print("Done! Open " + name + "/index.html")
+    with open(f'final/html-{lang}/scripts/words.js', 'w') as file:
+        file.write("const words = " + json.dumps(words,
+                                                 separators=(',', ':')) + ";")
+
+    print(f"Done! Open final/html-{lang}/index.html")
+
 
 if __name__ == '__main__':
     main()
--- html-pl-template/scripts/script.js
+++ html-pl-template/scripts/script.js
@@ -3,14 +3,14 @@ const listElement = document.getElementById("word-list");
 const paginationHeight = document.querySelector(".pagination").offsetHeight;
 const maxItems = listElement.offsetHeight / (listElement.children[0].offsetHeight + 4);
 const options = {
-   valueNames: ["word", "english", "id"],
+   valueNames: ["word", "translation", "id"],
    page: Math.floor(maxItems),
    pagination: true,
    indexAsync: true,
    pagination: {
       innerWindow: 1,
-      left: 0,
-      right: 0
+      left: 3,
+      right: 3
    },
    item: listElement.children[0].outerHTML
 };
@@ -33,36 +33,68 @@ listElement.addEventListener("click", e => {
 
    let id = "";
    if (e.target.tagName == "LI")
-        id = e.target.querySelector(".id").innerHTML;
+      id = e.target.querySelector(".id").innerHTML;
    else id = e.target.parentElement.querySelector(".id").innerHTML;
    selectWord(id);
 });
 
-function selectWord(id) {
-   // Set the basic ones
-   for (const child of main.children) {
-      if (child.tagName == "H3") break; // Stop at definitions and all that
-         let content = words[id][child.className];
+function goToReference() {
+   const result = words.find(x => x.word == this.innerHTML);
+   if (result !== undefined)
+      selectWord(result.id)
 
-         if (child.className == "types" && content.length > 0) {
-            child.innerHTML = content.join(", ");
-         } else if (content != undefined) {
-            child.innerHTML = content;
-         } else {
-            child.innerHTML = "";
-         }
+   return false;
+}
+
+function selectWord(id) {
+   const word = words[id];
+
+   main.querySelector(".word").innerHTML = word.word;
+   main.querySelector(".type").innerHTML = word.type;
+   main.querySelector(".ipa").innerHTML = `[${word.ipa}]`;
+   main.querySelector(".translation").innerHTML = word.translation;
+
+   if ('pl' in word) {
+      main.querySelector(".pl").innerHTML = `<span class="sc" title="W formie liczby mnogiej">pl</span> ${word.pl} <span class="ipa">[${word.pl_speech}]</span>`;
+   } else {
+      main.querySelector(".pl").innerHTML = '';
    }
 
-   // Definition
-   const definitions = words[id].definitions;
-   const definitionElement = main.querySelector(".definition");
-   definitionElement.innerHTML = "";
+   if ('fem' in word) {
+      if (word.fem != "FEM")
+         main.querySelector(".fem").innerHTML = `<span class="sc" title="W formie rodzaju żeńskiego">fem</span> ${word.fem} <span class="ipa">[${word.fem_speech}]</span>`;
+      else
+         main.querySelector(".fem").innerHTML = '<span class="sc" title="To słowo występuje tylko w rodzaju żeńskim">fem</span>';
+   } else {
+      main.querySelector(".fem").innerHTML = '';
+   }
 
-   for (let i = 0; i < definitions.length; i++) {
-      const type = words[id].types[i];
-      const definition = words[id].definitions[i];
-      if (type && definition)
-         definitionElement.innerHTML += `${definition}<br />`;
+   if ('pst' in word) {
+      main.querySelector(".pst").innerHTML = `<span class="sc" title="W formie czasu przeszłego">pst</span> ${word.pst} <span class="ipa">[${word.pst_speech}]</span>`;
+   } else {
+      main.querySelector(".pst").innerHTML = '';
+   }
+
+   if ('comp' in word) {
+      main.querySelector(".comp").innerHTML = `<span class="sc" title="W stopniu wyższym">comp</span> ${word.comp} <span class="ipa">[${word.comp_speech}]</span>`;
+   } else {
+      main.querySelector(".comp").innerHTML = '';
+   }
+
+   if ('supl' in word) {
+      main.querySelector(".supl").innerHTML = `<span class="sc" title="W stopniu najwyższym">supl</span> ${word.supl} <span class="ipa">[${word.supl_speech}]</span>`;
+   } else {
+      main.querySelector(".supl").innerHTML = '';
+   }
+
+   // note
+   const notes = words[id].notes;
+   const noteElement = main.querySelector(".note");
+   noteElement.innerHTML = "";
+
+   for (let i = 0; i < notes.length; i++) {
+      const note = words[id].notes[i];
+      noteElement.innerHTML += `<p>${note}</p>`;
    }
 
    // Example
@@ -71,20 +103,33 @@ function selectWord(id) {
    exampleElement.innerHTML = "";
 
    for (let i = 0; i < examples.length; i++) {
-      if (examples[i] == null) continue;
-      const type = words[id].types[i];
-      const typeExamples = words[id].examples[i];
-      if (type != undefined)
-         exampleElement.innerHTML += `<h4>${type}</h4>`;
-
-      for (example of typeExamples) {
-         exampleElement.innerHTML += `${example.join("<br />")}<br /><br />`;
-      }
+      const ex = examples[i].split('. ')
+      exampleElement.innerHTML += `<h4>„${ex[0]}“. — ${ex[1]}</h4>`;
    }
 
    // hide if empty
-   const definitionsState = definitions.length != 0 ? "" : "none";
-   const examplesState = examples.length != 0 ? "" : "none";
-   main.querySelector(".definitions-title").style.display = definitionsState;
+   const isAdj = word.type == 'adj' && 'comp' in word ? '' : 'none';
+   main.querySelector(".adj").style.display = isAdj;
+
+   const isFem = word.type == 'n' && 'fem' in word ? '' : 'none';
+   main.querySelector(".fem").style.display = isFem;
+
+   const isPl = word.type == 'n' && 'pl' in word ? '' : 'none';
+   main.querySelector(".pl").style.display = isPl;
+
+   const isPst = word.type == 'v' && 'pst' in word ? '' : 'none';
+   main.querySelector(".pst").style.display = isPst;
+
+   const notesState = word.notes.length != 0 ? "" : "none";
+   const examplesState = word.examples.length != 0 ? "" : "none";
+   main.querySelector(".notes-title").style.display = notesState;
    main.querySelector(".examples-title").style.display = examplesState;
+
+   // attach reference links
+   const links = main.querySelectorAll("a.see");
+   if (links != null) {
+      for (let i = 0; i < links.length; i++) {
+         links[i].addEventListener("click", goToReference);
+      }
+   }
 };
