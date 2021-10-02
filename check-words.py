c31b4d96212b64d83f28b3ed8b9742df41aa0c5e
Marcin Badurowicz <m@badurowicz.net>
2021-10-02 13:21:12 +0200

    Allow check-words to check a single word

--- check-words.py
+++ check-words.py
@@ -4,17 +4,10 @@
 import dictionaryparser
 import sys
 
-# read and parse dictionary file
-words = dictionaryparser.read_dictionary('./dictionary.csv')
-
 # all syllabes
 alls = [line.rstrip() for line in open('./syllabes.txt', encoding='utf-8')]
 
-# strict mode: fail on first problem and return exitcode 1 so CI will know
-# about it
-strict = True if len(sys.argv) > 1 and sys.argv[1] == "strict" else False
-
-def check_syllabes(speech, org):
+def check_syllabes(speech, org, strict=False):
     if speech == "FEM": return # ignore "FEM" syllabes, as it is only placeholder
     l = speech.strip().replace("ˈ", "")
     for i in l.split('.'):
@@ -22,21 +15,39 @@ def check_syllabes(speech, org):
             print(f"SYLLABLE WARN: {l} (because of {i}); WORD: {org}")
             if strict: exit(1)
 
-# check all words except empty speech and names for proper syllabes
-for i in filter(lambda x: x['speech'] != '' and x['type'] != 'name', words):
-    check_syllabes(i['speech'], i['word'])
+def check_dictionary(strict=False):
+    # read and parse dictionary file
+    words = dictionaryparser.read_dictionary('./dictionary.csv')
 
-for i in filter(lambda x: 'pst_speech' in x, words):
-    check_syllabes(i['pst_speech'], i['word'])
+    # check all words except empty speech and names for proper syllabes
+    for i in filter(lambda x: x['speech'] != '' and x['type'] != 'name', words):
+        check_syllabes(i['speech'], i['word'], strict)
 
-for i in filter(lambda x: 'pl_speech' in x, words):
-    check_syllabes(i['pl_speech'], i['word'])
+    for i in filter(lambda x: 'pst_speech' in x, words):
+        check_syllabes(i['pst_speech'], i['word'], strict)
 
-for i in filter(lambda x: 'fem_speech' in x, words):
-    check_syllabes(i['fem_speech'], i['word'])
+    for i in filter(lambda x: 'pl_speech' in x, words):
+        check_syllabes(i['pl_speech'], i['word'], strict)
 
-for i in filter(lambda x: 'comp_speech' in x, words):
-    check_syllabes(i['comp_speech'], i['word'])
+    for i in filter(lambda x: 'fem_speech' in x, words):
+        check_syllabes(i['fem_speech'], i['word'], strict)
 
-for i in filter(lambda x: 'supl_speech' in x, words):
-    check_syllabes(i['supl_speech'], i['word'])
+    for i in filter(lambda x: 'comp_speech' in x, words):
+        check_syllabes(i['comp_speech'], i['word'], strict)
+
+    for i in filter(lambda x: 'supl_speech' in x, words):
+        check_syllabes(i['supl_speech'], i['word'], strict)
+
+
+if __name__ == "__main__":
+    # strict mode: fail on first problem and return exitcode 1 so CI will know
+    # about it
+    strict = True if len(sys.argv) == 2 and sys.argv[1] == "strict" else False
+    
+    # single mode: check the syllabes given as the second argument
+    single = True if len(sys.argv) == 3 and sys.argv[1] == "single" else False    
+
+    if not single:
+        check_dictionary(strict)
+    else:
+        check_syllabes(sys.argv[2], "stdin")
